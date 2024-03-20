7627f066a8c81f0ab30be7f86d0f4839b6757bbc
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-03-20 22:26:37 +0900

    before_action にメソッドをまとめる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -17,11 +20,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
 def update
-  @post = Post.find(params[:id])
   if @post.update(post_params)
     redirect_to posts_path
   else
@@ -30,7 +31,6 @@ def update
 end
 
 def destroy
-  @post = Post.find(params[:id])
   @post.destroy
   redirect_to posts_path
 end
@@ -40,5 +40,11 @@ private
 def post_params
   params.require(:post).permit(:title, :content)
 end
+
+def set_post
+  @post = Post.find(params[:id])
+
+end
+
 
 end
