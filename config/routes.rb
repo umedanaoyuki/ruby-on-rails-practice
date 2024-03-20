8c79c361d297efbec4193559c4c51385806101d1
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-03-20 20:22:09 +0900

    追加のルートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   # root "posts#index"
   #
   # localhost:3000/posts/
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
 end
